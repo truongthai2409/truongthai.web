63f2660447b3f9bfce0fc4ac52883257dab43447
Thai <truongthongthethai240902@gmail.com>
2025-02-16 22:00:53 +0700

    Update DataProject.jsx

--- src/utils/DataProject.jsx
+++ src/utils/DataProject.jsx
@@ -7,7 +7,7 @@ export const ProjectName = {
 export const ProjectURL = {
     pops: 'https://pops.onrender.com/',
     figma: 'https://www.figma.com/file/bMhPqy2RoV8LYoB4XD4ccV/Prototype?type=design&node-id=0%3A1&mode=design&t=j5OjdLSO94H9zCYf-1',
-    movie: 'https://truongthai2409.github.io/MovieApp/'
+    movie: 'https://mephim.vercel.app/'
 };
 export const ProjectImage = {
     pops: 'images/POPS.png',
